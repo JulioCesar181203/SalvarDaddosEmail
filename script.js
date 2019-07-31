e719bedf23d0dd651144db045e171981216c7fb1
juliocesar <jubslopes1812@hotmail.com>
2019-07-31 18:00:53 -0300

    Salvar dados de email

---
+++ script.js
@@ -0,0 +1,38 @@
+let email1 = document.querySelector("#email")
+let email2 = document.querySelector("#email2")
+let senha = document.querySelector("#senha")
+let nome = document.querySelector("#Nome")
+let sexo = document.querySelector("#sexo")
+let botao = document.querySelector("button")
+let texto = document.querySelector("h2")
+let div = document.querySelector("div")
+
+
+
+
+function cadastrar(){
+if(email1 === email2){
+    localStorage.setItem("nome", nome.value)
+    localStorage.setItem("email1", email1.value)
+    localStorage.setItem("email2", email2.value)
+    localStorage.setItem("senha", senha.value)
+    localStorage.setItem("sexo", sexo.value)
+    }
+    
+    else{
+        
+        alert("ERRO ENCONTRADO");
+    }
+        
+        
+        // div.style.display = "none"
+        // texto.innerHTML = `Bem vindo ${localStorage.nome}`;
+    }
+
+
+
+
+
+
+
+botao.onclick = cadastrar;
